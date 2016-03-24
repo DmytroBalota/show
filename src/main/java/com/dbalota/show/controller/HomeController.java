165d83fe4ed22f9c981568481a94996ad9bbe963
DmytroBalota <Dmytro_Balota@epam.com>
2016-03-24 19:09:21 +0200

    Added spring security to application with two users in memory

--- src/main/java/com/dbalota/show/controller/HomeController.java
+++ src/main/java/com/dbalota/show/controller/HomeController.java
@@ -7,13 +7,40 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/")
-    public ModelAndView test(HttpServletResponse response) throws IOException {
-        return new ModelAndView("home","now",new Date());
+    public ModelAndView home(HttpServletResponse response) throws IOException {
+        return new ModelAndView("home", "now", new Date());
+    }
+
+    //Spring Security see this :
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login(
+            @RequestParam(value = "error", required = false) String error,
+            @RequestParam(value = "logout", required = false) String logout) {
+
+        ModelAndView model = new ModelAndView();
+        if (error != null) {
+            model.addObject("error", "Invalid username and password!");
+        }
+
+        if (logout != null) {
+            model.addObject("msg", "You've been logged out successfully.");
+        }
+        model.setViewName("login");
+
+        return model;
+
+    }
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public ModelAndView logout() {
+        return new ModelAndView("login", "_csrf", null);
     }
 }
---
+++ src/main/java/com/dbalota/show/security/VelocitySecurity.java
@@ -0,0 +1,85 @@
+package com.dbalota.show.security;
+
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class VelocitySecurity {
+
+    /**
+     * Gets the user name of the user from the Authentication object
+     *
+     * @return the user name as string
+     */
+    public static String getPrincipal() {
+        Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (obj instanceof UserDetails) {
+            return ((UserDetails) obj).getUsername();
+        } else {
+            return "Guest";
+        }
+    }
+
+    /**
+     * Is the user granted all of the grantedAuthorities passed in
+     *
+     * @param roles a string array of grantedAuth
+     * @return true if user has all of the listed authorities/roles, otherwise false
+     */
+    public static boolean allGranted(String[] checkForAuths) {
+        Set<String> userAuths = getUserAuthorities();
+        for (String auth : checkForAuths) {
+            if (userAuths.contains(auth))
+                continue;
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Is the user granted any of the grantedAuthorities passed into
+     *
+     * @param roles a string array of grantedAuth
+     * @return true if user has any of the listed authorities/roles, otherwise false
+     */
+    public static boolean anyGranted(String[] checkForAuths) {
+        Set<String> userAuths = getUserAuthorities();
+        for (String auth : checkForAuths) {
+            if (userAuths.contains(auth))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * is the user granted none of the supplied roles
+     *
+     * @param roles a string array of roles
+     * @return true only if none of listed roles are granted
+     */
+    public static boolean noneGranted(String[] roles) {
+        Set<String> userAuths = getUserAuthorities();
+        for (String auth : roles) {
+            if (userAuths.contains(auth))
+                return false;
+        }
+        return true;
+    }
+
+    private static Set<String> getUserAuthorities() {
+        Object obj = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Set<String> roles = new HashSet<String>();
+        if (obj instanceof UserDetails) {
+            Collection gas = ((UserDetails) obj).getAuthorities();
+            for (Object ga : gas) {
+                roles.add(((GrantedAuthority) ga).getAuthority());
+            }
+        }
+        return roles;
+    }
+}
